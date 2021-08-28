5db0cd5faa3c587e2e96e9802a7eafc459cabf0b
Lakshmi Prashanth <86674577+prashanth-io@users.noreply.github.com>
2021-08-28 18:14:09 +0530

    Time: 32 ms (63.12%), Space: 20.1 MB (62.35%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_map<int,int>map;
+        for(auto it:nums)
+        {
+            map[it]++;
+            if(map[it]>1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
