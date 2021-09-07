3ab9faca29bf23859fc053b72facb333b48afe0d
Lakshmi Prashanth <86674577+prashanth-io@users.noreply.github.com>
2021-09-08 02:54:52 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (59.48%) - LeetHub

---
+++ jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        
+        int count=0;
+        for(char c:stones)
+        {
+            
+            if(jewels.find(c) !=string::npos )
+            {
+                count++;
+            }
+        }
+        return count;
+        
+        
+    }
+};
